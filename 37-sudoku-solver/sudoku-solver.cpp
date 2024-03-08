274e11205ad175ea83faf05c505e1b91cc52f39b
Anushka Mokashi <100767949+AnushkaJMokashi@users.noreply.github.com>
2024-03-08 23:57:41 +0530

    Time: 17 ms (29.30%) | Memory: 7.7 MB (57.93%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -27,16 +27,16 @@ public:
 
     bool isValid(vector<vector<char>>& board, int row, int col,char c){
         for (int i = 0; i < 9; i++) {
-    if (board[i][col] == c)
-      return false;
+            if (board[i][col] == c)
+                return false;
 
-    if (board[row][i] == c)
-      return false;
+            if (board[row][i] == c)
+                return false;
 
-    if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
-      return false;
-  }
-  return true;
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+                return false;
+        }
+        return true;
     }
 };
 
